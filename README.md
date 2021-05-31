4825e31a779ac5059911b50ad9eff15d03f0362b
pqui42 <pquiring42@gmail.com>
2021-05-31 22:15:19 +0200

    added now examples
    Signed-off-by: pqui42 <pquiring42@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # chapter_5
 ## Rvalue Refernces, Move Semantics, and Perfect Forwarding
 ### Item 23: Understand std::move and std::forward
-#### Summary
+#### Things to Remember
+- std::move performs an unconditional cast to an rvalue. In and of itself, it dosen't move anything.
+- std::forward cast its arguments to an rvalue only if that argument is bound to an rvalue.
+- Neither std::move nor std::forward do anything at runtime.
+- move request on const objects are treated as copy request.
--- moveAndForward.cpp
+++ moveAndForward.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
 
 class String
 {
@@ -21,7 +22,40 @@ class Annotation
         String m_text;
 };
 
+class A
+{
+    public:
+        A (int &n) { std::cout << "lvalue overload n=" << n << std::endl;} // copy ctor rvalue
+        A (int && n) {std::cout << "rvalue overload n=" << n << std::endl;} // move ctor
+};
 
+// https://en.cppreference.com/w/cpp/utility/forward
+class B
+{
+    public:
+        template<class T1, class T2, class T3>
+        B(T1 && t1, T2 && t2, T3 && t3) :
+            _a1{std::forward<T1>(t1)},
+            _a2{std::forward<T2>(t2)},
+            _a3{std::forward<T3>(t3)}
+        {
+        }
+    private:
+    std::string _a2;
+    A _a1, _a3;
+};
+
+template<class T, class U>
+std::unique_ptr<T> make_unique1(U && u)
+{
+    return std::unique_ptr<T>(new T(std::forward<U>(u)));
+}
+
+template<class T, class... U>
+std::unique_ptr<T> make_unique2(U&&... u)
+{
+    return std::unique_ptr<T>(new T(std::forward<U>(u)...));
+}
 
 int main()
 {
@@ -31,4 +65,11 @@ int main()
     String a(std::move(const_str)); //copy ctor of string -> don't declare objects const if you want to be able to move from them
     const String b(std::move(str)); // move ctor
     Annotation ano(std::move(b)); //
+
+    auto p1 = make_unique1<A>(2); // rvalue;
+    int i = 1;
+    auto p2 = make_unique1<A>(i); // lvalue;
+
+    std::cout << "B" << std::endl;
+    auto p3 = make_unique2<B>(1,"hi",2);
 }
